cfdb335d14804b997266b676fac4c2dca6e404d8
Shyamal <scghoshbc@gmail.com>
2023-07-29 22:50:06 -0700

    cURL fetched

--- express_server.js
+++ express_server.js
@@ -20,3 +20,9 @@ app.listen(PORT, () => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+// sending HTML
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
