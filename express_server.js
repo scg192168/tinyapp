2e97967eceb1887a23f684b86f80c347164f8421
Shyamal <scghoshbc@gmail.com>
2023-07-31 23:41:10 -0700

    url shortening part 1

--- express_server.js
+++ express_server.js
@@ -18,6 +18,8 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.use(express.urlencoded({ extended: true }));
+
 // add router 
 
 app.get("/urls.json", (req, res) => {
@@ -51,7 +53,18 @@ app.get("/hello", (req, res) => {
   res.render("hello_world", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {}
