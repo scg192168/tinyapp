5d0deea014821d030e32196c23e8cd8bb1ccc578
Shyamal <scghoshbc@gmail.com>
2023-08-02 18:13:54 -0700

    Deleting URLs implemented

--- express_server.js
+++ express_server.js
@@ -67,7 +67,14 @@ app.post("/urls", (req, res) => {
   res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
-function generateRandomString() {}
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id]
+  res.redirect("/urls")
+});
+
+function generateRandomString() {
+  
+}
 
 
 app.get("/u/:id", (req, res) => {
