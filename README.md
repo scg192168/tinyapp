fdccdc9d52b59b93db978468eb5a6c50cea4e41f
Shyamal <scghoshbc@gmail.com>
2023-08-05 12:58:41 -0700

    Loging page implemented

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # tinyapp
 tinyapp project
+# TinyApp Project
+
+TinyApp is a full stack web application built with Node and Express that allows users to shorten long URLs (à la bit.ly).
+
+## Final Product
+
+!["Login page"](https://github.com/hktaneja/tinyapp/blob/master/docs/Login.PNG)
+!["URLs page"](https://github.com/hktaneja/tinyapp/blob/master/docs/Urls.PNG)
+!["TinyURL page"](https://github.com/hktaneja/tinyapp/blob/master/docs/TinyUrl.PNG)
+
+## Getting Started
+
+- Install all dependencies (see below) using the `npm install` command.
+- Run the development web server using the `npm start` command.
+
+## How to Use
+
+- Run your node server
+- Browse to `http://localhost:8080/`
+- Enjoy all its features!
+
+## Dependencies
+
+- Node.js
+- Express
+- EJS
+- bcrypt
+- body-parser
+- cookie-session
+- mocha (for tests)
+- chai (for tests)
--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const app = express();
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const PORT = 8080; // default port 8080
@@ -11,19 +11,21 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 const getUserByEmail = (email) => {
-const existingUser = Object.values(users).find(user => user.email === email);
-return existingUser;
+  const existingUser = Object.values(users).find(
+    (user) => user.email === email
+  );
+  return existingUser;
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-// add router 
+// add router
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -35,13 +37,6 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-//app.get("/urls", (req, res) => {
-//   console.log("urlDatabase", urlDatabase);
-//     const templateVars = { urls: urlDatabase, userName };
-//     console.log({templateVars});
-//   res.render("urls_index", templateVars);
-// });
-
 app.get("/hello", (req, res) => {
   const templateVars = { greeting: "Hello World!" };
   res.render("hello_world", templateVars);
@@ -52,7 +47,10 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -62,25 +60,25 @@ app.post("/urls", (req, res) => {
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id]
-  res.redirect("/urls")
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
 });
 
 app.get("/u/:id", (req, res) => {
-  const longURL = "http://www.lighthouselabs.ca"
+  const longURL = "http://www.lighthouselabs.ca";
   res.redirect(longURL);
 });
 
-
 function generateRandomString(length) {
-  const alphanumericChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  
+  const alphanumericChars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * alphanumericChars.length);
     result += alphanumericChars.charAt(randomIndex);
   }
-  
+
   return result;
 }
 
@@ -88,110 +86,102 @@ function generateRandomString(length) {
 const randomString = generateRandomString(6);
 console.log(randomString);
 
-
-// Serve the login form page 
-app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
 // POST endpoint for handling login
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   const { username } = req.body;
 
-  // Set the cookie named "username" with the value from the request body
-  res.cookie('username', username);
+// Set the cookie named "username" with the value from the request body
+  res.cookie("username", username);
 
-  // Redirect the browser back to the /urls page
-  res.redirect('/urls');
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+// Redirect the browser back to the /urls page
+  res.redirect("/urls");
 });
 
 // Sample user data (this should come from your authentication system)
 const users = {
-  "user123": {
+  user123: {
     id: "user123",
     username: "john_doe",
   },
 };
 
+// GET /login endpoint
 app.get("/login", (req, res) => {
-  // Assume the user logs in and sets the cookie with their user ID
-  const username = "user123";
-  res.cookie("username", username);
-  res.redirect("/urls");
+  res.render("login");
 });
 
 app.get("/urls", (req, res) => {
-  const username = req.cookies['username'];
+  const username = req.cookies["username"];
   const user = users[username];
 
   const templateVars = {
-    username: user ? user.username+'hellos' : null, urlDatabase
+    username: user ? user.username + "hellos" : null,
+    urlDatabase,
     // ... any other vars
   };
   res.render("urls_index", templateVars);
 });
 
 // Define your /logout route
-app.post('/logout', (req, res) => {
+app.post("/logout", (req, res) => {
   // Clear the username cookie to log out the user
-  res.clearCookie('username');
+  res.clearCookie("username");
 
   // Redirect the user back to the /urls page
-  res.redirect('/urls');
+  res.redirect("/urls");
 });
 
 // GET endpoint for /register
-app.get('/register', (req, res) => {
-  const username = req.cookies['username'];
-  const templateVars = {username}
-  res.render('registration', templateVars);
+app.get("/register", (req, res) => {
+  const username = req.cookies["username"];
+  const templateVars = { username };
+  res.render("registration", templateVars);
 });
 
 // Global user object (you may have this in your existing code)
 let user = {};
 
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
   const { email, password } = req.body;
 
   // Check if email or password is missing
   if (!email || !password) {
-    return res.status(400).json({ error: 'Email and password are required.' });
+    return res.status(400).json({ error: "Email and password are required." });
   }
-// Check if the email is already registered
+  // Check if the email is already registered
 
-if (getUserByEmail(email)) {
-  return res.status(400).json({ error: 'Email is already registered.' });
-}
+  if (getUserByEmail(email)) {
+    return res.status(400).json({ error: "Email is already registered." });
+  }
 
-// Generate a random user ID
+  // Generate a random user ID
   const userID = generateRandomString(6);
 
-// Create the new user object
+  // Create the new user object
   const newUser = {
     id: userID,
     email: email,
-    password: password // Remember, this is plain text for now (will be fixed later)
+    password: password, // Remember, this is plain text for now (will be fixed later)
   };
   users[userID] = newUser;
 
   // Return a success response
-  res.status(200).json({ message: 'Registration successful.' });
+  res.status(200).json({ message: "Registration successful." });
 });
-  // Add the new user to the global users object
-  //users[username] = newUser;
+// Add the new user to the global users object
+//users[username] = newUser;
 //});
 
-  app.get('/profile', (req, res) => {
-    const username = req.cookies.username;
-    // Fetch the specific user object using the user_id cookie value
-    const user = users[req.cookies.user_id];
-  
-    // Render the profile template and pass the entire user object to the template
-    res.render('profile', { user });
-  });
-  
+app.get("/profile", (req, res) => {
+  const username = req.cookies.username;
+  // Fetch the specific user object using the user_id cookie value
+  const user = users[req.cookies.user_id];
+
+  // Render the profile template and pass the entire user object to the template
+  res.render("profile", { user });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
